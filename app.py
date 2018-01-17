d3c2a23d352f85c50d5b8e021853250a4eaacd67
rid9 <p.ridho@yahoo.co.id>
2018-01-18 00:07:43 +0700

    Better formatting

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 # coding=utf-8
 
-from flask import Flask, request, abort, send_file, render_template, redirect, session
+from flask import Flask, request, render_template, session
 import hashlib
-from model import *
+import model
 
 app = Flask(__name__)
-
 app.secret_key = "SOMETHING SHOULD BE SECRET"
+
 
 def loggedIn(bool):
     def decorator(func):
         def wrap(*args, **kwargs):
             if not bool:
-                stat = session.get('loggedIn') == None
+                stat = session.get('loggedIn') is None
             else:
-                stat = session.get('loggedIn') != None
+                stat = session.get('loggedIn') is not None
                 print(stat)
             if (stat):
                 return func(*args, **kwargs)
@@ -28,15 +28,18 @@ def loggedIn(bool):
         return wrap
     return decorator
 
+
 @app.before_request
 def _db_connect():
-    if db.is_closed():
-        db.connect()
+    if model.db.is_closed():
+        model.db.connect()
+
 
 @app.teardown_request
 def _db_close(exc):
-    if not db.is_closed():
-        db.close()
+    if not model.db.is_closed():
+        model.db.close()
+
 
 @app.route("/", methods=["GET", "POST"])
 @loggedIn(False)
@@ -49,14 +52,20 @@ def login():
             m = hashlib.md5()
             m.update(request.form['password'].encode('utf-8'))
             password = m.hexdigest()
-            user = User.get((User.email == data['email']) & (User.password == password))
+            print(password)
+            user = model.User.get(
+                    (model.User.email == data['email']) &
+                    (model.User.password == password)
+                    )
             session['loggedIn'] = True
             session['user'] = user.fullName
 
-            return render_template("dashboard.html", success = "Successfully logged in.")
+            return render_template("dashboard.html",
+                                   success="Successfully logged in.")
         except Exception as e:
             print(e)
             return render_template("login.html", msg="Wrong email/password.")
+
 
 @app.route('/logout')
 @loggedIn(True)
@@ -65,10 +74,12 @@ def logout():
     session.pop('user', None)
     return render_template("login.html")
 
+
 @app.route("/dashboard")
 @loggedIn(True)
 def dashboard():
     return render_template("dashboard.html")
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
